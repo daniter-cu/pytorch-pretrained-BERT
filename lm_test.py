dfe181d419fcb020d618ae29daec91310a45ac0f
Dan Iter <daniter@stanford.edu>
2019-01-14 22:04:54 -0800

    torchafy softmax

--- lm_test.py
+++ lm_test.py
@@ -39,6 +39,30 @@ def calc_prob(context, question):
     perplexity = np.exp(entropy)
     return perplexity
 
+
+def calc_prob_nn(context, question):
+    gt_question = question
+    gt_q_tokens = tokenizer.tokenize(gt_question)
+    gt_indexed_q_tokens = tokenizer.convert_tokens_to_ids(gt_q_tokens)
+
+    context_tokens = tokenizer.tokenize(context)
+    indexed_context_tokens = tokenizer.convert_tokens_to_ids(context_tokens)
+
+    tokens_tensor = torch.tensor([indexed_context_tokens + gt_indexed_q_tokens])
+    segments_tensors = torch.tensor([0] * len(indexed_context_tokens) + [1] * len(gt_indexed_q_tokens))
+    predictions = model(tokens_tensor, segments_tensors)
+
+    total = 0
+    context_len = len(context_tokens)
+    q_len = len(gt_indexed_q_tokens)
+    for i in range(q_len):
+        preds = predictions[0, context_len + i]
+        m = torch.nn.LogSoftmax(0)
+        total += -m(preds)[gt_indexed_q_tokens[i]]
+    entropy = total / q_len
+    perplexity = torch.exp(entropy)
+    return perplexity.item()
+
 answerable_probs = []
 unanswerable_probs = []
 counter = 0
@@ -51,7 +75,7 @@ for i in range(len(data)):
             question = qas[j]['question']
             label = qas[j]['is_impossible']
             try:
-                prob = calc_prob(context, question)
+                prob = calc_prob_nn(context, question)
             except:
                 continue
             if label:
