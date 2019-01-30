83afb7b3dbee1f7b6b768d5560c9831cdae87c15
Dan Iter <daniter@stanford.edu>
2019-01-30 15:44:07 -0800

    added bert + lstm

---
+++ examples/dan_run_autoreg_local.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+python run_autoreg_train.py \
+  --bert_model bert-base-uncased \
+  --do_train \
+  --train_file ../dataset/train-v2.0.json \
+  --output_dir ../autoreg_model \
+  --num_train_epochs 1.0 \
+  --learning_rate 3e-5 \
+  --train_batch_size 32 \
+  --max_seq_length 128 \
+  --test_run
