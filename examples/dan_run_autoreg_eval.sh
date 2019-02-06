5e3cc88977ab737f10b1917606315c6add08894b
Dan Iter <daniter@stanford.edu>
2019-02-06 15:53:28 -0800

    fixed run

--- examples/dan_run_autoreg_eval.sh
+++ examples/dan_run_autoreg_eval.sh
@@ -2,6 +2,7 @@
 
 python run_autoreg_eval.py \
   --bert_model bert-base-uncased \
-   --eval_file ../../Squad2Generative/data/dev-v2.0.json \
+  --eval_file ../../Squad2Generative/data/dev-v2.0.json \
+  --output_dir deleteme \
   --train_batch_size 16 \
   --max_seq_length 128 
