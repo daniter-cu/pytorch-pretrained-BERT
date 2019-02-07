4d1d69a3936a9a38803535ead807fd0ec40f7341
Dan Iter <iter.dan@gmail.com>
2019-02-06 17:07:11 -0800

    update params

--- examples/dan_run_autoreg_eval.sh
+++ examples/dan_run_autoreg_eval.sh
@@ -4,5 +4,6 @@ python run_autoreg_eval.py \
   --bert_model ../autoreg_model/pytorch_model.bin \
   --eval_file ../../Squad2Generative/data/dev-v2.0.json \
   --output_dir deleteme \
+  --output_file losses.csv \
   --train_batch_size 1 \
   --max_seq_length 128 
