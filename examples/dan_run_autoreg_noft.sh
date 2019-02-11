e9298ef1a5e91017e360e892c0290e6999c19bbb
Dan Iter <iter.dan@gmail.com>
2019-02-10 16:30:14 -0800

    fixed params

--- examples/dan_run_autoreg_noft.sh
+++ examples/dan_run_autoreg_noft.sh
@@ -5,8 +5,8 @@ python run_autoreg_train.py \
   --do_train \
   --train_file ../../Squad2Generative/data/train-v2.0.json \
    --eval_file ../../Squad2Generative/data/dev-v2.0.json \
-  --output_dir ../autoreg_model_lm \
-  --num_train_epochs 2.0 \
+  --output_dir ../autoreg_model_lm_noft \
+  --num_train_epochs 10.0 \
   --learning_rate 3e-5 \
   --train_batch_size 16 \
   --max_seq_length 128 
