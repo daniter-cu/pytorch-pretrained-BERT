8f3539c0938fa46895e8c24d1248d47209505dea
Dan Iter <daniter@stanford.edu>
2019-03-12 22:47:09 -0700

    fixed chunk runner

--- examples/dan_run_chunk.sh
+++ examples/dan_run_chunk.sh
@@ -4,6 +4,8 @@ python run_chunk_lm_finetune.py \
   --bert_model bert-base-uncased \
   --do_train \
   --train_file ../../Squad2Generative/data/train-v2.0.json \
+  --chunk_file_train ../training_data_chunks.pkl \
+  --chunk_file_test ../val_data_chunks.pkl \
   --output_dir ../model_chunk \
   --num_train_epochs 10.0 \
   --learning_rate 3e-5 \
