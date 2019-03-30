765f9bc8c4ff28e73c3b5190c8345fa5aa71b401
Dan Iter <daniter@stanford.edu>
2019-03-29 19:59:15 -0700

    quick fix

--- examples/dan_run_cond_multihint.sh
+++ examples/dan_run_cond_multihint.sh
@@ -7,7 +7,7 @@ python train_cond_with_hint.py \
   --train_file ../../Squad2Generative/data/train-v2.0.json \
   --dev_file ../../Squad2Generative/data//dev-v2.0.json \
   --question_parts_file ../qparts/part_labels%s.pkl \
-  --output_dir ../model_hint \
+  --output_dir ../model_hint2 \
   --num_train_epochs 10.0 \
   --learning_rate 3e-5 \
   --train_batch_size 16 \
