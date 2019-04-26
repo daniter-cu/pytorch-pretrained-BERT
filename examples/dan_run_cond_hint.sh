ec2c0a990213b0218a69c58e7a78325a3a880eb3
Dan Iter <daniter@stanford.edu>
2019-04-25 22:55:43 -0700

    ready to train

--- examples/dan_run_cond_hint.sh
+++ examples/dan_run_cond_hint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-python train_cond_with_hint.py \
+python train_cond_with_copy.py \
   --bert_model bert-base-uncased \
   --do_train \
   --train_file ../../Squad2Generative/data/train-v2.0.json \
   --dev_file ../../Squad2Generative/data//dev-v2.0.json \
-  --question_parts_file ../qparts/part_labels%s.pkl \
-  --output_dir ../model_hint \
+  --question_parts_file ../qparses2/parsed_qs_labels%s.pkl \
+  --output_dir ../model_copy \
   --num_train_epochs 10.0 \
   --learning_rate 3e-5 \
   --train_batch_size 16 \
--- examples/dan_run_cond_hint_local.sh
+++ examples/dan_run_cond_hint_local.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-python train_cond_with_hint.py \
+python train_cond_with_copy.py \
   --bert_model bert-base-uncased \
   --do_train \
-  --multihint \
   --train_file ../dataset/train-v2.0.json \
   --dev_file ../dataset/dev-v2.0.json \
-  --question_parts_file ../qparts/part_labels%s.pkl \
-  --output_dir ../model_hint \
+  --question_parts_file ../qparts/copy_parts2/parsed_qs_labels%s.pkl \
+  --output_dir ../model_copy \
   --num_train_epochs 1.0 \
   --learning_rate 3e-5 \
   --train_batch_size 16 \
   --max_seq_length 256 \
   --test_run
+
+#   --multihint \
