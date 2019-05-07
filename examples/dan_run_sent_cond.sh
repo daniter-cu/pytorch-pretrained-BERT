994d494dfb4632d60cb91e5919d9cc16e6d80434
Dan Iter <daniter@stanford.edu>
2019-05-06 18:36:03 -0700

    new cond model and a bunch of tests

---
+++ examples/dan_run_sent_cond.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+python train_sent_cond.py \
+  --bert_model bert-base-uncased \
+  --do_train \
+  --train_file ../../Squad2Generative/data/train-v2.0.json \
+  --dev_file ../../Squad2Generative/data//dev-v2.0.json \
+  --question_parts_file ../qparses2/parsed_qs_labels%s.pkl \
+  --output_dir ../model_sent \
+  --num_train_epochs 10.0 \
+  --learning_rate 3e-5 \
+  --train_batch_size 16 \
+  --max_seq_length 256
--- examples/dan_run_sent_cond_local.sh
+++ examples/dan_run_sent_cond_local.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-python train_cond_with_copy.py \
+python train_sent_cond.py \
   --bert_model bert-base-uncased \
   --do_train \
   --train_file ../dataset/train-v2.0.json \
   --dev_file ../dataset/dev-v2.0.json \
   --question_parts_file ../qparts/copy_parts2/parsed_qs_labels%s.pkl \
-  --output_dir ../model_copy \
+  --output_dir ../model_sent \
   --num_train_epochs 1.0 \
   --learning_rate 3e-5 \
   --train_batch_size 16 \
