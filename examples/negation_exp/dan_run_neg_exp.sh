0331f49c0bb5b8d74719d1fc55922b76437fc6b9
Dan Iter <daniter@stanford.edu>
2019-05-22 20:41:18 -0700

    new runner files

---
+++ examples/negation_exp/dan_run_neg_exp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+python train_negation_exp.py \
+  --bert_model bert-base-uncased \
+  --do_train \
+  --train_file ../../negation_training_data.pkl \
+  --dev_file ../../negation_test_data.pkl \
+  --output_dir ../../model_neg_exp \
+  --num_train_epochs 10.0 \
+  --learning_rate 3e-5 \
+  --train_batch_size 16 \
+  --max_seq_length 256
